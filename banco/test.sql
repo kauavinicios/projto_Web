c3ce288e209c017a41579739987c789b3bd36326
kauavinicios <kauavinicios0k@gmail.com>
2022-12-08 22:17:25 -0400

    corrigindo banco

---
+++ banco/test.sql
@@ -0,0 +1,327 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 09-Dez-2022 às 03:15
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `adocao`
+--
+
+CREATE TABLE `adocao` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `raca` varchar(50) DEFAULT NULL,
+  `sexo` varchar(50) DEFAULT NULL,
+  `idade` date DEFAULT NULL,
+  `foto` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `adocao`
+--
+
+INSERT INTO `adocao` (`id`, `nome`, `raca`, `sexo`, `idade`, `foto`) VALUES
+(3, 'thor', 'pit bull', 'Macho', '2020-06-18', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `animal`
+--
+
+CREATE TABLE `animal` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `idade` date NOT NULL,
+  `raca` varchar(50) NOT NULL,
+  `sexo` varchar(20) NOT NULL,
+  `foto` int(200) DEFAULT NULL,
+  `cod_dono` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `animal`
+--
+
+INSERT INTO `animal` (`id`, `nome`, `idade`, `raca`, `sexo`, `foto`, `cod_dono`) VALUES
+(16, 'Alice', '2019-02-06', 'gato vira-lata', 'Fêmea', NULL, 1),
+(17, 'Galego', '2020-03-20', 'pit bull', 'Macho', NULL, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `telefone` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nome`, `cpf`, `telefone`) VALUES
+(1, 'kaua', '12345678910', 999999999),
+(2, 'rafael', '12345678911', 999999991),
+(5, 'Bruno', '12345678922', 925361221);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `telefone` int(11) NOT NULL,
+  `endereco` varchar(50) NOT NULL,
+  `cod_loja` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `funcionario`
+--
+
+INSERT INTO `funcionario` (`id`, `nome`, `cpf`, `telefone`, `endereco`, `cod_loja`) VALUES
+(1, 'kelvin', '0192873', 14233621, 'moreninha', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `loja`
+--
+
+CREATE TABLE `loja` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `endereco` varchar(50) NOT NULL,
+  `contato` int(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `loja`
+--
+
+INSERT INTO `loja` (`id`, `nome`, `endereco`, `contato`) VALUES
+(4, 'pet shop do tio joão', 'ali na esquina', 991234567),
+(6, 'pet shop prometeus', 'centro', 51421236);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `nota`
+--
+
+CREATE TABLE `nota` (
+  `id` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_loja` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `qtd` int(11) NOT NULL,
+  `data_compra` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `nota`
+--
+
+INSERT INTO `nota` (`id`, `id_cliente`, `id_loja`, `id_produto`, `qtd`, `data_compra`) VALUES
+(4, 1, 4, 6, 1, '2019-06-04');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `preco` double NOT NULL,
+  `descricao` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `nome`, `preco`, `descricao`) VALUES
+(5, 'ração de cachorro', 80, 'ração de cachorro de grande porte'),
+(6, 'ração de gato', 70, 'ração para gatos adultos');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `email`, `senha`) VALUES
+(0, 'kaua@gmail.com', 'e10adc3949ba59abbe56e057f20f883e');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `adocao`
+--
+ALTER TABLE `adocao`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `animal`
+--
+ALTER TABLE `animal`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_animal_cliente` (`cod_dono`);
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_funcionario_loja` (`cod_loja`);
+
+--
+-- Índices para tabela `loja`
+--
+ALTER TABLE `loja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `nota`
+--
+ALTER TABLE `nota`
+  ADD PRIMARY KEY (`id`,`id_cliente`,`id_loja`,`id_produto`),
+  ADD KEY `fk_nota_cliente` (`id_cliente`),
+  ADD KEY `fk_nota_loja` (`id_loja`),
+  ADD KEY `fk_nota_produto` (`id_produto`);
+
+--
+-- Índices para tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `adocao`
+--
+ALTER TABLE `adocao`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `animal`
+--
+ALTER TABLE `animal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `loja`
+--
+ALTER TABLE `loja`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `nota`
+--
+ALTER TABLE `nota`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `animal`
+--
+ALTER TABLE `animal`
+  ADD CONSTRAINT `fk_animal_cliente` FOREIGN KEY (`cod_dono`) REFERENCES `cliente` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD CONSTRAINT `fk_funcionario_loja` FOREIGN KEY (`cod_loja`) REFERENCES `loja` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `nota`
+--
+ALTER TABLE `nota`
+  ADD CONSTRAINT `fk_nota_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_nota_loja` FOREIGN KEY (`id_loja`) REFERENCES `loja` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_nota_produto` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
